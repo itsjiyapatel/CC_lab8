030dc199a7f44992444e792976157540f9d89d30
Jiya Patel <jiyapatel@Jiyas-MacBook-Air.local>
2025-03-13 15:59:45 +0530

    PES2UG22CS236

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,12 @@
 //File: hello.cpp
-#include <iostream>
+#include <iostream>git add hello.cpp
+
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout <<"Changed:)";
   return 0;
 }
